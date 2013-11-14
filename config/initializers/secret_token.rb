d9ee448ba9d1ed41d00b3511f4dedd88cb8928a1
Felipe Hangen <felipehangen@gmail.com>
2013-11-14 17:53:11 -0600

    Created the mms project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mms::Application.config.secret_token = '240999d408e604e22a4277609e07364cf4a41bd6951f3004433d38754154fe3303d303063feaf6be5050888ad8be2bb619c8266550ef8b2db4a6a2a3e90640ca'
